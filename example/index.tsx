b223d3953cb143a7db5086af6eb4018e0f9a60f2
Dirceusljr <dirjunior18@gmail.com>
2024-04-22 19:24:25 -0300

    aula 01

--- example/index.tsx
+++ example/index.tsx
@@ -1,14 +1,14 @@
 import 'react-app-polyfill/ie11';
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import { Thing } from '../.';
+import { AbBotao } from '../.';
 
 const App = () => {
   return (
     <div>
-      <Thing />
+      <AbBotao />
     </div>
-  );
-};
+  )
+}
 
 ReactDOM.render(<App />, document.getElementById('root'));
